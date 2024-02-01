2ea5ce4ba8a967f84cb76464a226f762ca9342b3
lxy01128250 <lxy01128250@alibaba-inc.com>
2024-02-01 15:21:08 +0800

    Java根据字段去重

--- src/main/java/com/beauty/aide/utils/CommonUtil.java
+++ src/main/java/com/beauty/aide/utils/CommonUtil.java
@@ -3,6 +3,10 @@ package com.beauty.aide.utils;
 import org.springframework.util.DigestUtils;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 /**
  * @author xiaoliu
@@ -16,5 +20,17 @@ public class CommonUtil {
     public static String md5(String content, String salt) {
         return md5(String.format("%s%s", content, salt));
     }
+
+    /**
+     * 根据特定字段去重
+     * @param keyExtractor
+     * @return
+     * @param <T>
+     */
+    public static <T> Predicate<T> distinctByKey(Function<? super T, ?> keyExtractor) {
+        Map<Object, Boolean> seen = new ConcurrentHashMap<>();
+        return t -> seen.putIfAbsent(keyExtractor.apply(t), Boolean.TRUE) == null;
+    }
+
 
 }
